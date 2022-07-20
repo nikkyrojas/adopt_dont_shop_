292111029772380a5d73e5e90b91893676769383
Nikky Rojas <rojas7313@gmail.com>
2022-07-20 15:19:05 -0600

    complete add pet feature

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,10 +1,13 @@
 class PetApplicationsController < ApplicationController
     def index
-
-
     end
-    #     @applications = Application.all
-    #     @pets = PetApplication.all
-    # end
+
+    def create
+        @application = Application.find(params[:application_id])
+        @pet = Pet.find(params[:pet_id])
+
+        PetApplication.create!(pet_id: @pet.id, application_id: @application.id)
+        redirect_to "/applications/#{@application.id}"
+    end
 end
 
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -10,32 +10,43 @@ RSpec.describe 'the show page' do
       @pet_app1 = PetApplication.create(pet_id: @pet_1.id, application_id: @application_1.id)
   end
 
-  it "show the applicantion with attributes such as name address status and description" do
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
-    
+  it "show the application with attributes such as name address status and description" do
     visit "/applications/#{@application_1.id}"
 
     expect(page).to have_content(@application_1.applicant_name)
     expect(page).to have_content(@application_1.street_address.upcase)
     expect(page).to have_content(@application_1.description)
     expect(page).to have_content(@application_1.status)
-    expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(pet_2.name)
   end
 
-  it "has a link, link goes to each pets show page" do
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
-    pet_3 = Pet.create(adoptable: true, age: 1, breed: 'pit', name: 'Lobely', shelter_id: @shelter_1.id)
+  it "pet results has a link, link goes to each pets show page" do
+    luna = Pet.create(adoptable: true, age: 1, breed: 'poodle', name: 'Luna', shelter_id: @shelter_1.id)
 
     visit "/applications/#{@application_1.id}"
     
-    fill_in 'Search', with: "Lob"
+    fill_in 'Search', with: "Luna"
     click_on("Search")
 
-    within "#addpets-#{pet_2.id}" do
-      expect(page).to have_link("Bare-y Manilow")
-      click_on("Bare-y Manilow")
-      expect(page).to have_current_path("/pets/#{@pet_2.id}")
+    within "#pets-#{luna.id}" do
+      expect(page).to have_link("Luna")
+      click_on("Luna")
+      expect(page).to have_current_path("/pets/#{luna.id}")
+    end
+  end
+
+  it "pets added has a link, link goes to each pets show page" do
+    luna = Pet.create(adoptable: true, age: 1, breed: 'poodle', name: 'Luna', shelter_id: @shelter_1.id)
+
+    visit "/applications/#{@application_1.id}"
+    
+    fill_in 'Search', with: "Luna"
+    click_on("Search")
+    click_on("Add Pet")
+
+    within "#addpets-#{luna.id}" do
+      expect(page).to have_link("Luna")
+      click_on("Luna")
+      expect(page).to have_current_path("/pets/#{luna.id}")
     end
   end
 
@@ -85,34 +96,40 @@ RSpec.describe 'the show page' do
   end
 
   it "adopt this pet button adds a pet to the application" do
-    application_2 = Application.create!(applicant_name: 'Bob Ross', street_address: '8753 Main St', city: 'Longmont', state: 'CO', zip_code: '80765', description: 'I make a lot of money', status: 'rejected')
-    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter_1.id)
-    
-    visit "/applications/#{application_2.id}"
+   dani = Application.create!(
+                 applicant_name: 'Dani Coleman',
+                 street_address: '912 Willow St',
+                 city: 'Arvada',
+                 state: 'Colorado',
+                 zip_code: '80003',
+                 status: 'In Progress')
+    foothills = Shelter.create!(name: 'Foothills Animal Shelter', city: 'Golden, CO', foster_program: false, rank: 9)
+    vida = Pet.create!(adoptable: true, age: 4, breed: 'Yorkshire', name: 'Vida', shelter_id: foothills.id)
+    visit "/applications/#{dani.id}"
     
     expect(page).to have_content("Add a Pet to this Application")
 
-    fill_in 'Search', with: 'Lobster'
+    fill_in 'Search', with: 'vida'
     click_button 'Search'
     click_button 'Add Pet'
 
     # application_2.add_pet
-    expect(current_path).to eq("/applications/#{application_2.id}")
-    expect(page).to have_content("Lobster")
+    expect(current_path).to eq("/applications/#{dani.id}")
+    expect(page).to have_content("Vida")
   end
 
-  it 'displays section to input description and submit application when dog count > 0' do
-    visit "/applications/#{@application_1.id}"
+  # it 'displays section to input description and submit application when dog count > 0' do
+  #   visit "/applications/#{@application_1.id}"
 
-    # fill_in 'Search', with: 'Lobster'
-    # click_button 'Submit'
-    # click_button 'Adopt this Pet'
+  #   # fill_in 'Search', with: 'Lobster'
+  #   # click_button 'Submit'
+  #   # click_button 'Adopt this Pet'
 
-    fill_in 'description', with: 'Testing'
-    click_button 'Submit Application'
+  #   fill_in 'description', with: 'Testing'
+  #   click_button 'Submit Application'
 
-    save_and_open_page
-    expect(page).to have_content("Pending")
-    expect(page).to have_content("Lobster")
-  end
+  #   save_and_open_page
+  #   expect(page).to have_content("Pending")
+  #   expect(page).to have_content("Lobster")
+  # end
 end
